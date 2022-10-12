371d7d3ecf76b5e5041667b23a7cc38a3786dcdb
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-10-13 02:52:45 +0300

    can do

--- 01-middlewares/bearerAuth.js
+++ 01-middlewares/bearerAuth.js
@@ -8,8 +8,8 @@ async function bearerAuth(req, res, next) {
   try {
     let bearerAuth = req.headers.authorization;    // `Bearer sdasdas.sdeeer.werwerwer`
     let token = bearerAuth.split(' ')[1];          // sdasdas.sdeeer.werwerwer
-    let userObject = jwt.verify(token, process.env.SECRET);  // {meow:"username" , "iat": "any"}
-    let user = await userModel.findOne({ where: { username: userObject.meow } });
+    let userObject = jwt.verify(token, process.env.SECRET);  // {fawzi:"username" , "iat": "any"}
+    let user = await userModel.findOne({ where: { username: userObject.fawzi } });
     if (user) {
       req.user = user;
       next();
